bd210fb7d830e03df6010cd6fa76dc8f3d02262e
avacosta <avacosta@ossesanjuan.com.ar>
2022-11-11 13:52:35 -0300

    hasta video 277

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+import { ValidatorsService } from 'src/app/shared/validator/validators.service';
+import { emailPattern, nombreApellidoPattern } from 'src/app/shared/validator/validaciones';
+import { noPuedeSerStrider } from '../../../shared/validator/validaciones';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
+    password: ['', [Validators.required, Validators.minLength(6) ]],
+    password2: ['', [Validators.required]]
+  },{
+    validators: [this.validatorService.camposIguales('password', 'password2')]
+  })
+  
+  
+  constructor( private fb: FormBuilder, private validatorService: ValidatorsService) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: 'Andrea Acosta',
+      email: 'test1@test.com',
+      username: 'aacosta'
+    })
+  }
+
+  camnpoNoValido(campo: string){
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario(){
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
   }
 
 }
