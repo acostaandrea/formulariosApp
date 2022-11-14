5de30a4d5fc6a50b79a4cc4e05fa93a54c9bd0ab
avacosta <avacosta@ossesanjuan.com.ar>
2022-11-14 09:50:24 -0300

    fin seccion 18

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 import { ValidatorsService } from 'src/app/shared/validator/validators.service';
 import { emailPattern, nombreApellidoPattern } from 'src/app/shared/validator/validaciones';
 import { noPuedeSerStrider } from '../../../shared/validator/validaciones';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -15,29 +16,61 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
     password: ['', [Validators.required, Validators.minLength(6) ]],
     password2: ['', [Validators.required]]
   },{
     validators: [this.validatorService.camposIguales('password', 'password2')]
-  })
+  });
+
+  // emailErrorMsg: string = '';
+
+  get emailErrorMsg(): string{
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors?.['required']){
+      return 'Email es obligatorio'
+    } else if( errors?.['pattern']){
+      return 'El valor ingresado no tiene formato de correo electrónico'
+    } else if( errors?.['emailTomado']){
+      return 'El mail ya fue usado'
+    } else{
+      return ''
+    }
+  }
   
   
-  constructor( private fb: FormBuilder, private validatorService: ValidatorsService) { }
+  constructor( private fb: FormBuilder, private validatorService: ValidatorsService, private emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
 
     this.miFormulario.reset({
       nombre: 'Andrea Acosta',
       email: 'test1@test.com',
-      username: 'aacosta'
+      username: 'aacosta',
+      password: 123456,
+      password2: 123456
     })
   }
 
   camnpoNoValido(campo: string){
     return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched
   }
+
+  // emailRequired(){
+  //   return this.miFormulario.get('email')?.errors?.['required'] && this.miFormulario.get('email')?.touched
+  // }
+
+
+  // emailFormato(){
+  //   return this.miFormulario.get('email')?.errors?.['pattern'] && this.miFormulario.get('email')?.touched
+  // }
+  
+  // emailTomado(){
+  //   return this.miFormulario.get('email')?.errors?.['emailTomado'] && this.miFormulario.get('email')?.touched
+  // }
+
+
 
   submitFormulario(){
     console.log(this.miFormulario.value);
